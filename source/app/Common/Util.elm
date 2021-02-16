251df646de137b1e33c19c7bd0fe9583566ee8bf
brahayan-dev <brahayan@live.com>
2021-02-15 22:02:39 -0500

    DEV | adding some funtions to toolkit

--- source/app/Common/Util.elm
+++ source/app/Common/Util.elm
@@ -1,6 +1,7 @@
-module Common.Util exposing (process, when, guard)
+module Common.Util exposing (guard, join, process, routine, when, pack)
 
 import Task
+
 
 when : Bool -> (m -> m) -> m -> m
 when isTrue modifier model =
@@ -22,6 +23,26 @@ process createElement wrapMsg model ( elementModel, globalState, elementCmd ) =
     , Cmd.map wrapMsg elementCmd
     )
 
+
+routine :
+    model
+    -> (localMsg -> msg)
+    -> (localModel -> model -> model)
+    -> ( localModel, Cmd localMsg )
+    -> ( model, Cmd msg )
+routine model wrapMsg updateModel ( localModel, localCmd ) =
+    ( updateModel localModel model, Cmd.map wrapMsg localCmd )
+
+pack : (model -> kind) -> state -> (model, cmd) -> (kind, state, cmd)
+pack toOpaque s (m, c) = (toOpaque m, s, c)
+
 guard : (a -> msg) -> a -> Cmd msg
 guard action value =
     Task.perform action (Task.succeed value)
+
+
+join : List ( localMsg -> msg, Cmd localMsg ) -> List (Cmd msg) -> Cmd msg
+join locals msgs =
+    Cmd.batch <|
+        List.append msgs <|
+            List.map (\( m, l ) -> Cmd.map m l) locals
--- source/app/Retail/Views/Shop.elm
+++ source/app/Retail/Views/Shop.elm
@@ -1,11 +1,12 @@
 module Retail.Views.Shop exposing (Model, Msg, init, update, view)
 
 import Browser.Navigation exposing (Key)
+import Common.Util as Util
 import Html exposing (Html)
-import Retail.Templates.Cabinet as Cabinet
+import Retail.Widgets.Cabinet as Cabinet
+import Retail.Widgets.Drawer as Drawer
 import Router exposing (redirectTo)
 import Store exposing (State)
-import Common.Util as Util
 
 
 type Model
@@ -13,30 +14,39 @@ type Model
         { basket : Int
         , globalState : State
         , navigationKey : Key
+        , drawerModel : Drawer.Model
         }
 
 
 init : ( Key, State ) -> ( Model, State, Cmd Msg )
 init ( key, state ) =
+    let
+        ( drawerModel, drawerCmd ) =
+            Drawer.init
+    in
     ( Model
         { basket = 0
         , globalState = state
         , navigationKey = key
+        , drawerModel = drawerModel
         }
     , state
-    , Util.guard NoOp <| Store.readUserIsActive state
+    , Util.join
+        [ ( DrawerMsg, drawerCmd ) ]
+        [ Util.guard NoOp <| Store.readUserIsActive state ]
     )
 
 
 type Msg
     = NoOp Bool
+    | DrawerMsg Drawer.Msg
 
 
 update : Msg -> Model -> ( Model, State, Cmd Msg )
 update msg (Model ({ navigationKey, globalState } as model)) =
     case msg of
         NoOp isOpen ->
-            (Model model
+            ( Model model
             , globalState
             , if isOpen then
                 Cmd.none
@@ -44,6 +54,11 @@ update msg (Model ({ navigationKey, globalState } as model)) =
               else
                 redirectTo navigationKey "/login"
             )
+
+        DrawerMsg localMsg ->
+            Drawer.update localMsg model.drawerModel
+                |> Util.routine model DrawerMsg (\l m -> { m | drawerModel = l })
+                |> Util.pack Model globalState
 
 
 view : Model -> Html Msg
